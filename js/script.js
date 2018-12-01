ef43e3b4da28e3a57180c47feed3f612fd8c4bd6
Tomer <25082829+truedl@users.noreply.github.com>
2018-12-01 20:45:36 +0200

    script.js [Build 002]

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 var height = window.innerHeight;
 var width = window.innerWidth;
+document.getElementById('profile').style.height = width/4 + 'px';
+document.getElementById('profile').style.borderRadius = width/4/2 + 'px';
+var fetch = document.querySelectorAll("[id='ebtn']");
+for(var i = 0; i < fetch.length; i++){
+  fetch[i].style.fontSize = width/25 + 'px';
+}
+
 pcontent = document.getElementById('p-content');
 pcontent.style.top = height/2-pcontent.offsetHeight/2 + 'px';
 pcontent.style.left = width/2-pcontent.offsetWidth/2 + 'px';
@@ -8,7 +15,7 @@ function smoothSlide(id){
   var slideto = document.getElementById(id).getBoundingClientRect().top;
   var plus = 1;
   var localVal = setInterval(function(){
-    if(plus < 14){
+    if(plus < 100){
       plus += 1;
     }
 
